f7866dde4aa927e8fef3eb563568249e67bea02d
JoaoFerrazfs <joaoferrazp@gmail.com>
2024-12-28 16:15:42 -0300

    docs: update oas

--- src/auth/oas/auth.schemas.ts
+++ src/auth/oas/auth.schemas.ts
@@ -16,27 +16,27 @@ export function AuthLoginDocs() {
 }
 
 const AuthLogin = {
-  required: ['email', 'pasword'],
+  required: ['email', 'password'],
   type: 'object',
   properties: {
-    access_token: {
-      type: 'string',
-      example:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXJuYW1lIjoiSm9hbyIsImVtYWlsIjoiam9hb0BnbWFpbC5jb20iLCJ0eXBlIjoiY3VzdG9tZXIiLCJpYXQiOjE3MzU0MDY3NTQsImV4cCI6MTczNTQwNzI1NH0.7zxeq0PJ4RSLEf1mObepyYGx7fhKhXLf8oG4Daj1gvs',
-    },
+    email: { type: 'string', example: 'joao@gmail124.com' },
+    password: { type: 'string', example: '12345678' },
   },
 };
 
 const AuthLoginResponse = {
-  required: ['title', 'name', 'type', 'phone'],
+  required: ['email', 'pasword'],
   type: 'object',
   properties: {
-    email: { type: 'string', example: 'joao@gmail124.com' },
-    name: { type: 'string', example: 'Joao' },
-    type: { type: 'string', example: 'customer' },
-    phone: {
-      type: 'string',
-      example: '31971694273',
+    data: {
+      type: 'object',
+      properties: {
+        access_token: {
+          type: 'string',
+          example:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXJuYW1lIjoiSm9hbyIsImVtYWlsIjoiam9hb0BnbWFpbC5jb20iLCJ0eXBlIjoiY3VzdG9tZXIiLCJpYXQiOjE3MzU0MDY3NTQsImV4cCI6MTczNTQwNzI1NH0.7zxeq0PJ4RSLEf1mObepyYGx7fhKhXLf8oG4Daj1gvs',
+        },
+      },
     },
   },
 };
